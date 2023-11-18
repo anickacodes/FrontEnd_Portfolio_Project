4eeb0dae7ec4ef3036ae451c73140d96388fd952
anickacodes <anickalewis@pursuit.org>
2023-11-18 05:25:13 -0500

    troubleshoot netlify load

--- src/components/StylesList.jsx
+++ src/components/StylesList.jsx
@@ -5,7 +5,7 @@ import '../styles/StylesList.css'
 
 const StylesList = () => {
   const navigate = useNavigate();
-  const { id } = useParams();
+  // const { id } = useParams();
   const API = import.meta.env.VITE_API_PORT;
   const [styles, setStyles] = useState([]);
 
