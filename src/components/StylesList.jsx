a6370397b063037a565cbcd66b7c9a6f2dba0486
anickacodes <anickalewis@pursuit.org>
2023-11-19 19:29:48 -0500

    backup data, styling cards

--- src/components/StylesList.jsx
+++ src/components/StylesList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import "../styles/StylesList.css";
+import localData from "../data/styles.json";
 
 const StylesList = () => {
   const navigate = useNavigate();
@@ -9,23 +10,43 @@ const StylesList = () => {
   const [styles, setStyles] = useState([]);
 
   useEffect(() => {
-    fetch(`${API}/styles`)
-      .then((r) => r.json())
-      .then((data) => setStyles(data))
-      .catch((error) => {
-        console.error(error);
+    const fetchLocalData = async () => {
+      try {
+        const res = await fetch(`${localData}`);
+        const myData = await res.json();
+        setStyles(myData);
+      } catch (err) {
+        console.error("Error fetching local data", err);
         navigate("/error");
+      }
+    };
+
+    fetch(`${API}/styles`)
+      .then((r) => {
+        if (!r.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return r.json();
+      })
+      .then((data) => setStyles(data))
+      .catch((apiError) => {
+        console.error("Error fetching from API", apiError);
+        fetchLocalData();
       });
-  }, []);
+  }, [API, navigate]);
 
   return (
     <div>
       <section className="stylesList-container" key={selectedStyleById}>
-        {styles.map(({id, category, service, image_url }) => (
+        {styles.map(({ id, category, service, image_url }) => (
           <div className="stylesIndex_card" key={id}>
             <Link to={`/styles/${id}`}>
               <img src={image_url} alt={category} />
-              <span>{service}</span>
+              {/* &nbsp; */}
+              <span>
+                Category: {category} <br />
+                Style: {service}
+              </span>
             </Link>
           </div>
         ))}
