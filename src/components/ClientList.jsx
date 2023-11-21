5017d5a298dcf93e48b0158657f32bd051a14c9b
anickacodes <anickalewis@pursuit.org>
2023-11-21 15:34:28 -0500

    button ⮐ ✅

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import "../styles/ClientList.css";
 
 const ClientList = () => {
   const {
     state: { style },
   } = useLocation();
-
+  const { id } = useParams();
   const API = import.meta.env.VITE_API_PORT;
   const [clients, setClients] = useState([]);
   const [fetchError, setFetchError] = useState(null);
@@ -27,42 +27,44 @@ const ClientList = () => {
         setFetchError(err.message);
       }
     };
-
     FetchClient();
   }, [API, style.id]);
 
   return (
     <>
       <div className="clientList_containter">
+        <button>
+          <Link to={`/styles/${id}`}> ⮐ </Link>
+        </button>
         <h2> Client List for</h2>
-        <b>Style:</b> {style.service} Appointments
+        <h3>Style: {style.service} Appointments </h3>
+        {fetchError ? (
+          <p>Error fetching data: {fetchError}</p>
+        ) : clients.length > 0 ? (
+          clients.map((client) => (
+            <div className="client_card" key={client.id}>
+              <p>
+                <b>Name:</b> {client.name}
+              </p>
+              <p>
+                <b>Contact:</b> {client.phone}{" "}
+              </p>
+              <p>
+                <b>Address:</b> {client.address}
+                {"N/A"}
+              </p>
+              <p>
+                <b>Rating:</b> {client.rating}{" "}
+              </p>
+              <p>
+                <b>Member:</b> {client.is_member ? "Yes🤍" : "No👀"}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>No clients found.</p>
+        )}
       </div>
-      {fetchError ? (
-        <p>Error fetching data: {fetchError}</p>
-      ) : clients.length > 0 ? (
-        clients.map((client) => (
-          <div className="client_card" key={client.id}>
-            <p>
-              <b>Name:</b> {client.name}
-            </p>
-            <p>
-              <b>Contact:</b> {client.phone}{" "}
-            </p>
-            <p>
-              <b>Address:</b> {client.address}{"N/A"}
-            </p>
-            <p>
-              <b>Rating:</b> {client.rating}{" "}
-            </p>
-            <p>
-              <b>Member:</b> {client.is_member ? "Yes🤍" : "No👀"}
-            </p>
-
-          </div>
-        ))
-      ) : (
-        <p>No clients found.</p>
-      )}
     </>
   );
 };
